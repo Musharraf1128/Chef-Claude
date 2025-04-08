57f54a6d301fbc7429b7db82168cd1bfc15132a7
musharraf <shahmusharaf1128@gmail.com>
2025-04-08 10:01:14 +0530

    added few more components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,41 @@
-export default function Main() {
+import { useState } from 'react'
+
+export default function App() {
+
+    const [ingredients, setIngredients] = useState([])
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get('ingredient')
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input
-                    type="text"
-                    placeholder="e.g. oregano" 
-                    aria-label="add-ingredient" 
+            <form action={addIngredient} className='add-ingredient-form'>
+                <input 
+                    type='text'
+                    placeholder='e.g. oregano'
+                    aria-label=''
+                    name='ingredient'                    
                 />
-                <button>Add ingredient</button>
+                <button>Add Ingredients</button>
             </form>
+        
+        {ingredientsListItems.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredientsListItems.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}
         </main>
     )
 }
