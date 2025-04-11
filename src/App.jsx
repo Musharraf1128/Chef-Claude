f87ebf695871517dc393a8a1ebe18edd5cb33e81
Musharraf <90516265+Musharraf1128@users.noreply.github.com>
2025-04-11 01:10:41 +0000

    added hugging face api key

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Header from "./components/Header"
 import Main from "./components/Main"
 
 export default function App() {
+    
+    const apiKey = import.meta.env.VITE_API_KEY;
+    
     return (
         <>   
             <Header />
